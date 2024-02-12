a5fa73015230c46daa50aed22c1231c8170c1b03
Jinesh15 <159221181+Jinesh15@users.noreply.github.com>
2024-02-12 22:16:03 +0530

    Add files via upload

---
+++ hug.js
@@ -0,0 +1,6 @@
+yesFunc = () => {
+    document.getElementById("button").remove();
+    document.getElementById("image2").classList.remove("hide"); 
+    document.getElementById("benefits").classList.remove("hide"); 
+    document.getElementById("image1").classList.add("hide");
+}
